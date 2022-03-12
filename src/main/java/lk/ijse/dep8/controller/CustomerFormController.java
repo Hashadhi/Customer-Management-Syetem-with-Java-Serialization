d565dc91caa87692b7c5abe08578d3e16ab5b2f1
Hashadhi <hashadhijayasinghe@gmail.com>
2022-03-12 20:55:11 +0530

    Add UI

---
+++ src/main/java/lk/ijse/dep8/controller/CustomerFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep8.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtPicture;
+    public TableView tblCustomers;
+
+    public void btnSaveCustomer_OnAction(ActionEvent actionEvent) {
+
+    }
+}
