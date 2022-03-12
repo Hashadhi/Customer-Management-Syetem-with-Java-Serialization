755bf12c54c6840de89a19c34921f1745ced6cb2
Hashadhi <hashadhijayasinghe@gmail.com>
2022-03-12 21:47:40 +0530

    Validate user inputs

--- src/main/java/lk/ijse/dep8/controller/CustomerFormController.java
+++ src/main/java/lk/ijse/dep8/controller/CustomerFormController.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep8.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -12,6 +13,28 @@ public class CustomerFormController {
     public TableView tblCustomers;
 
     public void btnSaveCustomer_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public boolean isValid() {
+        if (!txtID.getText().matches("C\\d{3}")) {
+            new Alert(Alert.AlertType.ERROR, "Invalid Id").show();
+            txtID.selectAll();
+            txtID.requestFocus();
+            return false;
+        } else if (!txtName.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Name cannot be empty").show();
+            txtName.selectAll();
+            txtName.requestFocus();
+            return false;
+        }else if (!txtAddress.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Address cannot be empty").show();
+            txtAddress.selectAll();
+            txtAddress.requestFocus();
+            return false;
+        }
+
+        return true;
 
     }
 }
